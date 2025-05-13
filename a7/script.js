e5055f008a936549ae9ce2910c901c70d3650e2c
kshreeya2006 <kshreeya2006@gmail.com>
2025-05-13 20:05:26 +0530

    Update

--- a7/script.js
+++ a7/script.js
@@ -1,4 +1,4 @@
-const getVal = id => parseFloat(document.getElementById(id).value);
+/*const getVal = id => parseFloat(document.getElementById(id).value);
 
 function result() {
   const a = getVal("t1"), b = getVal("t2");
@@ -9,5 +9,40 @@ function result() {
     Division: ${b !== 0 ? (a / b) : 'Cannot divide by zero'}
   `;
   document.getElementById("result").innerHTML = output;
-  console.log(output.replace(/<br>/g, '\n'));
 }
+*/
+
+const add= (a,b)=>{
+    return a+b;
+};
+
+const sub= (a,b)=>{
+    return a-b;
+};
+
+const mul= (a,b)=>{
+    return a*b;
+};
+
+const div= (a,b)=>{
+    return a/b;
+};
+
+const calc= ()=>{
+    let n1= parseFloat(document.getElementById("t1").value);
+    let n2= parseFloat(document.getElementById("t2").value);
+    let r1= add(n1,n2);
+    let r2= sub(n1,n2);
+    let r3= mul(n1,n2);
+    let r4= div(n1,n2);
+
+    const output = `
+    Addition: ${r1}<br>
+    Subtraction: ${r2}<br>
+    Multiplication: ${r3}<br>
+    Division: ${r4}
+  `;
+    
+  document.getElementById("result").innerHTML = output;
+}
+
