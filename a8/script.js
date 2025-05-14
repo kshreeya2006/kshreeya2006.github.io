7e79954d9095d85fe0dcc4e6d0cc3bd553fa0fd9
kshreeya2006 <kshreeya2006@gmail.com>
2025-05-14 19:16:14 +0530

    update

--- a8/script.js
+++ a8/script.js
@@ -1,26 +1,32 @@
 const add= ()=>{
     let n1= parseFloat(document.getElementById("t1").value);
     let n2= parseFloat(document.getElementById("t2").value);
-      document.getElementById("result").innerHTML = `Addition: ${n1 + n2}`;
+    let r= n1+n2;
+    const output = `Addition: ${r}<br></br>`;
+    document.getElementById("result").innerHTML = output;
 };
 
 const sub= ()=>{
     let n1= parseFloat(document.getElementById("t1").value);
     let n2= parseFloat(document.getElementById("t2").value);
-    document.getElementById("result").innerHTML = `Subtraction: ${n1 - n2}`;
+    let r= n1-n2;
+    const output = `Subtraction: ${r}<br></br>`;
+    document.getElementById("result").innerHTML = output;
+    
 };
 
 const mul= ()=>{
     let n1= parseFloat(document.getElementById("t1").value);
     let n2= parseFloat(document.getElementById("t2").value);
-    document.getElementById("result").innerHTML = `Multiplication: ${n1 * n2}`;
+    let r=n1*n2;
+    const output = `Multiplication: ${r}<br></br>`;
+    document.getElementById("result").innerHTML = output;
 };
 
 const div= ()=>{
     let n1= parseFloat(document.getElementById("t1").value);
     let n2= parseFloat(document.getElementById("t2").value);
-      if (n2 === 0) {
-    document.getElementById("result").innerHTML = `Cannot divide by zero`;
-  } else {
-    document.getElementById("result").innerHTML = `Division: ${n1 / n2}`;
+    let r= n1/n2;
+    const output = `Division: ${r}<br></br>`;
+    document.getElementById("result").innerHTML = output;
 };
