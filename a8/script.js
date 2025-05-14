d8637323c804acc78ab2403eb66ff719f1ff6472
kshreeya2006 <kshreeya2006@gmail.com>
2025-05-14 19:31:29 +0530

    update

--- a8/script.js
+++ a8/script.js
@@ -30,3 +30,40 @@ const div= ()=>{
     const output = `Division: ${r}<br></br>`;
     document.getElementById("result").innerHTML = output;
 };
+
+/** 
+const add = (a, b) => {
+  return a + b;
+};
+
+const sub = (a, b) => {
+  return a - b;
+};
+
+const mul = (a, b) => {
+  return a * b;
+};
+
+const div = (a, b) => {
+  return a / b;
+};
+
+const calc = (op) => {
+  let n1 = parseFloat(document.getElementById("n1").value);
+  let n2 = parseFloat(document.getElementById("n2").value);
+  switch (op) {
+    case "add":
+      output.innerHTML = "Addition: " + add(n1, n2);
+      break;
+    case "sub":
+      output.innerHTML = "Subtraction: " + sub(n1, n2);
+      break;
+    case "mul":
+      output.innerHTML = "Multiplication: " + mul(n1, n2);
+      break;
+    case "div":
+      output.innerHTML = "Division: " + div(n1, n2);
+      break;
+  }
+};
+**/
