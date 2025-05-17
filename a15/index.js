f40e543d54e261e5271f3c8cfd4a12ebb8c199c3
kshreeya2006 <kshreeya2006@gmail.com>
2025-05-17 19:42:02 +0530

    update

--- a15/index.js
+++ a15/index.js
@@ -1,20 +1,4 @@
-const items= [
-    {id:1, name:"Apple", price:40},
-    {id:2, name:"Banana", price:30},
-    {id:3, name:"Orange", price:80}
-];
 
-//item id and qty
-const cart={1:7, 3:5}
-
-items.map((value) => {
-  if (cart[value.id]) {
-    const total = value.price * cart[value.id];
-    console.log(value.id, value.name, value.price, cart[value.id], total);
-  }
-});
-
-/**
 const items= [
     {id:1, name:"Apple", price:40, qty:3, status:"pending"},
     {id:2, name:"Banana", price:30, qty:5, status:"pending"},
@@ -37,4 +21,3 @@ console.log(updatedItems);
 const filteredItems = updatedItems.filter(item => item.qty > 2).map(item => {
     console.log(`Name: ${item.name}, Price: ${item.price}, Qty: ${item.qty}, Total: ${item.total}`);
   });
-**/
