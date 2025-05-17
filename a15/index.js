f08aa0769d4d6c165e258b51cd6f00d0a296d742
kshreeya2006 <kshreeya2006@gmail.com>
2025-05-17 19:02:13 +0530

    update

--- a15/index.js
+++ a15/index.js
@@ -1,13 +1,14 @@
 const items= [
-    {name:"Apple", price:40, status:"pending"},
-    {name:"Banana", price:30, status:"pending"},
-    {name:"Orange", price:80, status:"pending"}
+    {name:"Apple", price:40, qty:3, status:"pending"},
+    {name:"Banana", price:30, qty:5, status:"pending"},
+    {name:"Orange", price:80, qty:2, status:"pending"}
 ];
 
 const updatedItems= items.map(item=>({
     ...item, 
     price: item.price+5,
-    status: "completed"
+    status: "completed",
+    total: (item.price+5)* item.qty
 }));
 
 console.log(updatedItems);
