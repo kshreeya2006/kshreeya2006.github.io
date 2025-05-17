b274246e4875334bd54e9a7bd4121e9fcd82d979
kshreeya2006 <kshreeya2006@gmail.com>
2025-05-17 19:31:35 +0530

    update

--- a15/index.js
+++ a15/index.js
@@ -1,10 +1,27 @@
 const items= [
-    {name:"Apple", price:40, qty:3, status:"pending"},
-    {name:"Banana", price:30, qty:5, status:"pending"},
-    {name:"Orange", price:80, qty:2, status:"pending"}
+    {id:1, name:"Apple", price:40},
+    {id:2, name:"Banana", price:30},
+    {id:3, name:"Orange", price:80}
 ];
 
-//updaate the price and status
+//item id and qty
+const cart={1:7, 3:5}
+
+items.map((value) => {
+  if (cart[value.id]) {
+    const total = value.price * cart[value.id];
+    console.log(value.id, value.name, value.price, cart[value.id], total);
+  }
+});
+
+/**
+const items= [
+    {id:1, name:"Apple", price:40, qty:3, status:"pending"},
+    {id:2, name:"Banana", price:30, qty:5, status:"pending"},
+    {id:3, name:"Orange", price:80, qty:2, status:"pending"}
+];
+
+//update the price and status
 const updatedItems= items.map(item=>({
     ...item, 
     price: item.price+5,
@@ -15,9 +32,9 @@ const updatedItems= items.map(item=>({
 //print name, price, qty, total where qty>2
 //Hint: use filter and map
 
-/**console.log(updatedItems);**/
+console.log(updatedItems);
 
 const filteredItems = updatedItems.filter(item => item.qty > 2).map(item => {
     console.log(`Name: ${item.name}, Price: ${item.price}, Qty: ${item.qty}, Total: ${item.total}`);
   });
-
+**/
