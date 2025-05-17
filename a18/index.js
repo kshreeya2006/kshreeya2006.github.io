95c933131044e57613a7647be49a6a537d6795ef
kshreeya2006 <kshreeya2006@gmail.com>
2025-05-17 20:55:15 +0530

    update

--- a18/index.js
+++ a18/index.js
@@ -1,6 +1,11 @@
 function add(x,y){
     return  new Promise ((resolve,reject)=>{
+        if(x<5){
+            reject("x cannot be less than 5");
+        }
+        else{
         resolve(x+y);
+        }
     });
 }
 
@@ -9,8 +14,13 @@ function sqr(a){
 }
 
 async function main(){
+    try{
     let sum= await add(4,5)
     let result= sqr(sum)
     console.log(result)
+    }
+    catch(error){
+        console.log(error);
+    }
 }
 main()
